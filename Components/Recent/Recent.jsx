c06ef4239f5e39e88fca49bdb6b82e5eceb6c824
Tanvir Hasan <57188940+Tanvir-art@users.noreply.github.com>
2024-01-11 21:32:27 +0600

    responsive

--- Components/Recent/Recent.jsx
+++ Components/Recent/Recent.jsx
@@ -21,7 +21,7 @@ const Recent = () => {
 
   return (
     <div className='py-10'>
-      <h2 className='text-3xl font-bold text-center'>Recent Post</h2>
+      <h2 className='text-2xl md:text-3xl font-bold text-center'>Recent Post</h2>
 
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  gap-6 py-10">
